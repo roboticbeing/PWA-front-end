a94b3eb4b0c6877f54d24a82136d6d8253c032cb
Rosario Cali <alex@Rosarios-MacBook-Pro.local>
2019-08-06 14:52:53 +0200

    added login page

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  handleSubmit() {
+    alert('New feature coming soon...');
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Email:
+            <input type='text' name='email' />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type='password' name='Password' />
+          </label>
+          <br />
+          <button type='submit' className='btn btn-primary'>
+            Log In
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
