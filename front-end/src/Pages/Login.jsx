511aec540b048c6c69ea1442427a0afa4441a693
Rosario Cali <alex@Rosarios-MacBook-Pro.local>
2019-08-06 14:42:46 +0200

    Added Bootstrap

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,14 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Login extends Component {
-  render () {
-    return ( 
-    
-    <div >
-      Login
-    </div>
-    
-    );
+  render() {
+    return <div>Login</div>;
   }
 }
 
