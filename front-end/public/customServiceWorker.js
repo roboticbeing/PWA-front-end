bbcc97b614c54d95f938feefd5aca95a6558351a
Rosario Cali <alex@Rosarios-MacBook-Pro.local>
2019-08-13 11:37:57 +0200

    import workbox in service-worker

--- front-end/public/customServiceWorker.js
+++ front-end/public/customServiceWorker.js
@@ -1,3 +1,12 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+
 // Cache name
 const CACHE_NAME = 'static-cache-v1';
 
