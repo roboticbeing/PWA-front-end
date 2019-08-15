decfc0baa9a3c836c292b19dd46a6016b342f57c
Martin Andersen <eaamarba@students.eaaa.dk>
2019-08-15 12:37:11 +0200

    servicewirker

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', ()=>{
+        navigator.serviceWorker.register('./customServiceWorker.js')
+    });
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+//serviceWorker.register();
